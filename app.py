c6c2fe0d3489af3a15ee574feabb0821d7cebf83
Daisuke Maki <lestrrat+github@gmail.com>
2016-08-08 15:45:33 +0900

    pass

--- app.py
+++ app.py
@@ -516,6 +516,7 @@ def conference_cfp_input():
         )
     except:
         # TODO: capture, and do the right thing
+        pass
 
     if not session:
         raise Exception(octav.last_error())
