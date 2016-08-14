f4ddbb836452d293db8bb3b82e76dd71c1c5ad84
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2016-08-14 17:19:18 +0900

    Avoid unnecessary no-op git diff in app.py #37

--- app.py
+++ app.py
@@ -38,14 +38,14 @@ class Config(object):
         with open(file, 'r') as f:
             self.cfg = json.load(f)
 
-        for section in ['OCTAV', 'GITHUB', 'REDIS_INFO', 'GOOGLE_MAP']:
+        for section in ['OCTAV', 'REDIS_INFO', 'GITHUB', 'GOOGLE_MAP']:
             if not self.cfg.get(section):
                 raise Exception( "missing section '" + section + "' in config file '" + file + "'" )
         if self.cfg.get('OCTAV').get('BASE_URI'):
             raise Exception(
                 'DEPRECATED: {"OCTAV":{"BASE_URI"}} in config.json is deprecated.\
- Please use {"OCTAV":{"endpoint"}} instead and remove {"OCTAV":{"BASE_URI"}}.')
-
+ Please use {"OCTAV":{"endpoint"}} instead and remove {"OCTAV":{"BASE_URI"}}.'
+        )
 
     def section(self, name):
         return self.cfg.get(name)
