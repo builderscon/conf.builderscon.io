d33c3bd94bfeba1b5566b021808a4613c03760b3
Daisuke Maki <lestrrat+github@gmail.com>
2017-05-09 20:51:41 +0900

    let staff images go through sharaq

--- blueprints/sharaq.py
+++ blueprints/sharaq.py
@@ -4,6 +4,7 @@ page = flask.Blueprint('sharaq', __name__)
 
 @page.route('/sharaq/<preset>/<path:url>')
 def sharaq(preset, url):
+    url = url.replace('?', '%3F')
     return flask.redirect('https://sharaq-dot-builderscon-1248.appspot.com/?preset=%s&url=%s' % (preset, url))
 
 
